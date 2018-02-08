8d48f5e243eb6d784cd28f2a6c676ec4faae4ff8
Kyle Cohen <spausek@gmail.com>
2018-02-08 15:39:46 -0800

    Restarted, still don't know what I'm doing

--- letters.js
+++ letters.js
@@ -1,9 +1,20 @@
-exports.letters = letters;
 
-function Letters(input) {
-	this.input = input;
-	this.display = false;
-	this.showLetter = function(){
-		if (this.input === ' ')
+module.exports = Letters;
+
+const Letters = function(word, guess){
+	this.word = word;
+	this.correctGuess = guess;
+	this.showWord = ' ';
+	this.displayWord = function(){
+		var emptyWord = '';
+	 	if(this.correctGuess === undefined){
+	 		for(var i = 0; i < this.word.length; i++){
+	 			emptyWord += '_';
+	 		}
+	 	}
+
 	}
+
+	
+
 }
