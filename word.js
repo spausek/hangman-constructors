1b769ed8b9343435c70422f35a4d02dde55e6ed8
Kyle Cohen <spausek@gmail.com>
2018-02-09 14:34:23 -0800

    Tested letters function. Not sure if its working. Going to build more in game logic and test it out more

--- word.js
+++ word.js
@@ -10,6 +10,44 @@ var Word = function(word) {
 		}
 	};
 
-}
+	this.findTheWord = function (){
+		var count = 0;
+		for (var i = 0; i < this.letters.length; i ++){
+			if (this.letters[i].display){
+				count++;
+			}
+
+		}
+
+			if (count === this.letters.length){
+				this.found = true;
+			}
+
+			return this.found;
+		
+	};
+
+	this.checkLetter = function(guessedLetter){
+		var returnThis = 0;
+		for (var i = 0; i < this.letters.length;){
+			if (this.letters[i].letter === guessedLetter){
+				this.letters[i].display = true;
+				returnThis++;
+			}
+		}
+
+		return returnThis;
+	}
+
+	this.wordDisplay = function(){
+		var hangmanWordDisplay = '';
+		for (var i = 0; i < this.lets.length; i++){
+			hangmanWordDisplay =+ this.letters[i].displayLetter();
+		}
+		return hangmanWordDisplay;
+
+	};
+
+};
 var foo = new Word('Apples');
-console.log(foo);
+console.log(foo);// maybe works?
