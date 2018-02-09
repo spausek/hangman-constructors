5d613cf790981d4e27f560c5a5c029c9ecfc2602
Kyle Cohen <spausek@gmail.com>
2018-02-09 00:58:18 -0800

    Working on word.js. Building a contructor function to push all the letters into an array and display them as underscores.

--- word.js
+++ word.js
@@ -1,7 +1,15 @@
-var letter = require('./letter.js');
+var letter = require('./letters.js');
 
-var Word = function{
+var Word = function(word) {
 	this.word = word;
 	this.letters = [];
-	
+	this.found = false;
+	this.getLetters = function (word) {
+		for (var i = 0; i < this.word.length; i++){
+			this.letters.push(new letter.Letter(this.word[i])); //should push the letters or underscores into the letters array
+		}
+	};
+
 }
+var foo = new Word('Apples');
+console.log(foo);
