5d3bd0fa749841c7a62cd4f1ea900feb8b89ddb9
Kyle Cohen <spausek@gmail.com>
2018-02-10 23:06:06 -0800

    Still error in word constructor

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ hangMan = {
 	wordArray: Letter.words,
 	startGame: function(word){
 		this.resetGuess();
-		this.selectedWord = new Word.Word(this.wordArray[Math.floor(Math.random()*this.wordArray.length)]);
+		this.selectedWord = new Word(this.wordArray[Math.floor(Math.random()*this.wordArray.length)]);
 		this.selectedWord.displayLetters();
 		console.log('Welcome to Around The World Hangman!');
 		console.log(this.selectedWord.hangManWordDisplay() + '\n');
@@ -22,8 +22,51 @@ hangMan = {
 	resetGuess: function(){
 		this.guesses = 8;
 	},
-	
-	
 
-}
-	
+	promptUser: function(){
+		prompt.get(['guessLetter'], function(err, result){
+			console.log('');
+			console.log('You guessed this letter: ' + result.guessLetter);
+
+			var userGuess = this.selectedWord.checkLetter(result.guessLetter);
+
+			if (userGuess === 0){
+				if (this.lettersGuessed.indexOf(result.guessLetter) < 0){
+					this.guessLetter.push(result.guessLetter);
+					this.guesses --;
+					console.log('Wrong Letter!');
+					}
+
+				else{
+					console.log('Correct!');
+				}
+
+				
+					if (this.selectedWord.findTheWord()) {
+						console.log('You got it! The country was ' + this.selectedWord);
+						console.log('Nice!')
+
+						return;
+					}
+				
+			}
+
+			 console.log('Guesses remaining: ', this.guesses);
+		    console.log(this.selectedWord.wordDisplay());
+				console.log("");
+		    console.log('Letters already guessed: ' + this.lettersGuessed);
+
+
+		    if ((this.guesses > 0) && (this.selectedWord.found === false)){
+		    	this.promptUser();
+		    } else if(this.guesses === 0){
+			    	console.log('You lost! The country was', this.selectedWord.word);
+			    	console.log('Try agina!');
+			  } else{
+			    	console.log(this.selectedWord.wordDisplay());
+			  }
+		});
+	}
+};
+
+hangMan.startGame();
